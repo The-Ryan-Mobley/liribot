a6aeb4608d869af189d91a41d9bef93c9f430d39
Ryan Mobley <ryan.mobley94@gmail.com>
2019-08-20 18:57:32 -0500

    large functionality, movie api complete

--- keys.js
+++ keys.js
@@ -1,17 +1,50 @@
 //https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+//spoitfy key 42ce09f7e58c4f6991de213473890d68
+//spotify secret 4652196a99b24dddbb4623b3eedcd418
+const fs = require("fs");
 const axios = require('axios');
-const liri = require("./liri.js");
+const Spotify = require('node-spotify-api');
 
+var obj = undefined;
+
+function readInput(){
+    fs.readFile("input.txt", "utf8", (err, data)=> {
+        if (err) {
+            return console.log(err);  
+        }
+        obj = JSON.parse(data);
+        takeActions(obj.field,obj.query);
+    });
+}
+function spotifyAPI(input){
+    var spotify = new Spotify({
+        id: "42ce09f7e58c4f6991de213473890d68",
+        secret: "4652196a99b24dddbb4623b3eedcd418"
+      });
+      spotify.search({ type: 'track', query: input, limit: 5 }, (err, data)=> {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+      });
+
+}
 function movieAPI(input) {
-    var movieName = input;
+    //let outArray = [];
+    let movieName = input;
     axios({
             method: 'get',
             url: 'http://www.omdbapi.com/?t=' + movieName + '&apikey=8fcc4e9e',
         })
-        .then(function (response) {
-            console.log(response);
+        .then((response)=> {
+            console.log(`Title: ${response.data.Title}`);
+            console.log(`Release Date: ${response.data.Title}`);
+            console.log(`IMDB Rating: ${response.data.Ratings[0].Value}`);
+            console.log(`Rotten Tomatoes Rating: ${response.data.Ratings[1].Value}`);
+            console.log(`Country: ${response.data.Country}`);
+            console.log(`Language: ${response.data.Language}`);
+            console.log(`Summary:\n ${response.data.Plot}`);
+            console.log(`Cast:\n ${response.data.Actors}`);   
         });
-        //need to get: title, release date, imdb rating, rotten tomatoes rating,COO, language,plot summary, and actors
 
 }
 function concertAPI(input){
@@ -20,25 +53,44 @@ function concertAPI(input){
             method: 'get',
             url: 'https://rest.bandsintown.com/artists/' + artist + '/events?app_id=codingbootcamp',
         })
-        .then(function (response) {
+        .then((response)=> {
+            console.log("here we go");
             console.log(response);
         });
         //need to get: date name of venue and venue location/address
 
 }
-//will eventually be mobed to liri file, will need to export query fields then import api data
-function main(){
-    let field = process.argv[2].toString().toLowerCase();
-    let input = process.argv[3].toString().toLowerCase();
+function takeActions(field, query){
     switch(field){
-        case "concert":{
-            concertAPI(input);
+        case "movie":{
+            movieAPI(query);
             break;
         }
-        case "movie":{
-            movieAPI(input);
+        case "concert":{
+            concertAPI(query);
+            break;
+        }
+        case "song":{
+            spotifyAPI(query);
             break;
         }
     }
+
+}
+function writeOutput(arr){
+    let passArr =[];
+    arr.forEach((index)=>{
+        passArr.push(index);
+
+    });
+    module.exports = passArr;
+    
+}
+function main(){
+    readInput();
+    module.exports = "sending......";
+    
+
+    
 }
 main();
--- liri.js
+++ liri.js
@@ -1,3 +1,43 @@
 require("dotenv").config();
 const keys = require("./keys.js");
 //var spotify = new Spotify(keys.spotify);
+const fs = require("fs");
+class userRequest{
+    constructor(f,q){
+        this.field = process.argv[2].toString().toLowerCase();
+        this.query = process.argv[3].toString().toLowerCase();
+    }
+}
+function writeInputs(){
+    let ur = new userRequest();
+    fs.writeFile("input.txt", JSON.stringify(ur), function(err) {
+        if (err) {
+            return console.log(err);
+          }
+    });
+   
+ 
+}
+function grabKeys(){
+    console.log(keys);
+}
+function readQuery(){
+    fs.readFile("querys.txt", "utf8", (err, data)=> {
+        if (err) {
+            return console.log(err);  
+        }
+        let output = data.split(',');
+        output.forEach((index)=>{
+            console.log(index);
+
+        });
+
+    });
+
+}
+function main(){
+    writeInputs();
+    grabKeys();
+    //readQuery();
+}
+main();
