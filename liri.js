9d20718f72d952d0941ae008e5a40236241f1b00
Ryan Mobley <ryan.mobley94@gmail.com>
2019-08-23 20:00:35 -0500

    changed code to follow assignment structure

--- liri.js
+++ liri.js
@@ -1,33 +1,40 @@
 require("dotenv").config();
 const inquirer = require("inquirer");
-
-//var spotify = new Spotify(keys.spotify);
 const fs = require("fs");
-class userRequest{
-    constructor(f,q){
+const axios = require('axios');
+const Spotify = require('node-spotify-api');
+const keys = require('./keys.js');
+const spotify = new Spotify(keys.spotify);
+
+
+class userRequest {
+    constructor(f, q) {
         this.field = f;
         this.query = q;
     }
 }
-function writeInputs(ur){
-    fs.writeFile("input.txt", JSON.stringify(ur), function(err) {
+var obj = undefined;
+
+function logInputs(ur) {
+    fs.appendFileSync('log.txt', JSON.stringify(ur), (err) => {
         if (err) {
             return console.log(err);
-          }
-        const keys = require("./keys.js");
-        
+        }
+
     });
 }
-function grabKeys(){
+
+function grabKeys() {
     console.log(keys);
 }
-function readQuery(){
-    fs.readFile("querys.txt", "utf8", (err, data)=> {
+
+function readQuery() {
+    fs.readFile("querys.txt", "utf8", (err, data) => {
         if (err) {
-            return console.log(err);  
+            return console.log(err);
         }
         let output = data.split(',');
-        output.forEach((index)=>{
+        output.forEach((index) => {
             console.log(index);
 
         });
@@ -35,26 +42,134 @@ function readQuery(){
     });
 
 }
-function promptUser(){
-    inquirer.prompt([{
-        type:'input',
-        message:'what do you want to find?',
-        default:'song',
-        name:'menu'
-    },
-    {
-        type:'input',
-        meessage:'Sure what do you want to look up?:',
-        default:'I want it that way',
-        name:'title'
 
-    }]).then((inq)=>{
-        let ur = new userRequest(inq.menu,inq.title);
-        writeInputs(ur);
+function promptUser() {
+    inquirer.prompt([{
+            type: 'input',
+            message: 'what can I do for you?',
+            name: 'menu'
+        },
+        {
+            type: 'input',
+            meessage: 'Sure what do you want to look up?:',
+            name: 'title'
+
+        }
+    ]).then((inq) => {
+        let list = [];
+        let fault = new userRequest('spotify-this', 'I want it that way');
+        list.push(fault);
+        let ur = new userRequest(inq.menu, inq.title);
+        list.push(ur);
+        let passReq = new userRequest('', '');
+        list.forEach((index) => {
+            passReq.field = index.field;
+            passReq.query = index.query;
+        });
+        takeActions(passReq.field,passReq.query);
+        logInputs(passReq);
     });
+
+    function takeActions(field, query) {
+        switch (field) {
+            case "movie-this": {
+                movieAPI(query);
+                break;
+            }
+            case "concert-this": {
+                concertAPI(query);
+                break;
+            }
+            case "spotify-this": {
+                spotifyAPI(query);
+                break;
+            }
+            default: {
+                ReadRandom();
+                break;
+
+            }
+        }
+
+    }
 }
-function main(){
+
+function spotifyAPI(input) {
+    spotify.search({
+        type: 'track',
+        query: input,
+        limit: 5
+    }, (err, data) => {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        console.log(data.tracks.items[0].artists[0].name);
+        console.log(data.tracks.items[0].name);
+        console.log(data.tracks.items[0].preview_url);
+        console.log(data.tracks.items[0].album.name);
+    });
+
+}
+
+function movieAPI(input) {
+    //let outArray = [];
+    let movieName = input;
+    axios({
+            method: 'get',
+            url: 'http://www.omdbapi.com/?t=' + movieName + '&apikey=8fcc4e9e',
+        })
+        .then((response) => {
+
+            console.log(`Title: ${response.data.Title}`);
+            console.log(`Release Date: ${response.data.Title}`);
+            console.log(`IMDB Rating: ${response.data.Ratings[0].Value}`);
+            console.log(`Rotten Tomatoes Rating: ${response.data.Ratings[1].Value}`);
+            console.log(`Country: ${response.data.Country}`);
+            console.log(`Language: ${response.data.Language}`);
+            console.log(`Summary:\n ${response.data.Plot}`);
+            console.log(`Cast:\n ${response.data.Actors}`);
+        });
+}
+
+function concertAPI(input) {
+    var artist = input;
+    axios({
+            method: 'get',
+            url: 'https://rest.bandsintown.com/artists/' + artist + '/events?app_id=codingbootcamp',
+        })
+        .then((response) => {
+            let concertArr = [];
+            for (let i = 0; i < 10; i++) {
+                concertArr.push(response.data[i]);
+            }
+            concertArr.forEach((index) => {
+                let date = grabDate(index.datetime);
+                console.log("Concert date: " + date);
+                console.log("Venue: " + index.venue.name);
+                console.log('City: ' + index.venue.city + ", " + index.venue.region);
+            });
+
+        });
+
+}
+function grabDate(string){
+    const moment = require('moment');
+    let dateMod = string.substr(0,10);
+    return(moment(dateMod,'YYYY-MM-DD').format('MM/DD/YYYY'));
+}
+function ReadRandom(){
+    fs.readFile('random.txt','utf-8',(err,data)=>{
+        if (err) {
+            return console.log(err);
+        }
+        let arr = data.split(',');
+        spotifyAPI(arr[1]);
+
+    })
+}
+
+function main() {
     promptUser();
-    
+
 }
 main();
